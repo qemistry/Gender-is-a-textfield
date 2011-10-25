0f23398c240fa024e88edb7c0701a67365fefe4b
Jesse Beach <splendidnoise@gmail.com>
2011-10-24 23:24:16 -0400

    Added a font from Google Fonts. Exported the theme settings to gender.info.
    Signed-off-by: Jesse Beach <splendidnoise@gmail.com>

--- themes/gender/template.php
+++ themes/gender/template.php
@@ -5,8 +5,18 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
+
+function gender_preprocess_html(&$variables) {
+  drupal_add_css('http://fonts.googleapis.com/css?family=Varela+Round',
+    array(
+      'type' => 'external',
+      'every_page' => TRUE,
+      'weight' => -100,
+    )
+  );
+}
